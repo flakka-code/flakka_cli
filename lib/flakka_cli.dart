4576073216db0a5f4c2bd67013c80c9e1f5266c3
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-29 12:59:39 -0500

    chore: initial commit

---
+++ lib/flakka_cli.dart
@@ -0,0 +1,10 @@
+/// flakka_cli, A CLI for developing flakka projects
+///
+/// ```sh
+/// # activate flakka_cli
+/// dart pub global activate flakka_cli
+///
+/// # see usage
+/// flakka_cli --help
+/// ```
+library;
